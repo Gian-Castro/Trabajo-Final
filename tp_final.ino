0ff4729bfdb5481950f75dbc6cb118c042013512
Gian Castro <gian.castro@alumnos.frm.utn.edu.ar>
2024-10-16 20:52:54 -0300

    Add files via upload

---
+++ tp_final.ino
@@ -0,0 +1,33 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define pinsen 3
+#define DHTTYPE DHT11
+#define ledok 11
+#define ledalar 12
+
+DHT sens(pinsen, DHTTYPE);
+
+float h;
+float t;
+
+void setup() {
+  Serial.begin(9600);
+  sens.begin();
+
+  pinMode(ledok, OUTPUT);
+  pinMode(ledalar, OUTPUT);
+}
+
+void loop() {
+  h = sens.readHumidity();
+  t = sens.readTemperature();
+
+  Serial.print("Humedad: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+
+}
